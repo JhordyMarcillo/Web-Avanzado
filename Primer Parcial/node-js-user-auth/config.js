e217f2a91a14c2761caa2ee89c000c827f1e3607
JhordyMarcillo <jhordypaulsb2@gmail.com>
2024-11-18 01:24:24 -0500

    update

--- Primer Parcial/node-js-user-auth/config.js
+++
@@ -1,7 +0,0 @@
-export const{
-    PORT = 3000,
-    SALT_ROUNDS = 3, 
-    SECRET_JWT_KEY = 'secret',
-    MONGO_URI = "mongodb+srv://JhordyM:JhordyM.@cluster0.fiselec.mongodb.net/"
-    
-} = process.env 
